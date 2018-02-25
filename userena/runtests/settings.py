fff7981a4f5a8df886d11c3bc8fac9aceb7a339c
James Meakin <jamesmeakin@gmail.com>
2018-02-25 13:09:57 +0100

    Remove compat for old django versions

--- userena/runtests/settings.py
+++ userena/runtests/settings.py
@@ -2,9 +2,6 @@
 DEBUG = True
 
 import os
-import sys
-
-import django
 
 settings_dir = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.abspath(settings_dir)
@@ -71,42 +68,28 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '_g-js)o8z#8=9pr1&amp;05h^1_#)91sbo-)g^(*=-+epxmt4kc9m#'
 
-
-if django.VERSION < (1, 8):
-    TEMPLATE_DEBUG = DEBUG
-
-    TEMPLATE_DIRS = (
-        os.path.join(PROJECT_ROOT, 'templates/'),
-    )
-
-    # List of callables that know how to import templates from various sources.
-    TEMPLATE_LOADERS = (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    )
-else:
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [
-                # insert your TEMPLATE_DIRS here
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
             ],
-            'APP_DIRS': True,
-            'OPTIONS': {
-                'context_processors': [
-                    # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                    # list if you haven't customized them:
-                    'django.contrib.auth.context_processors.auth',
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.i18n',
-                    'django.template.context_processors.media',
-                    'django.template.context_processors.static',
-                    'django.template.context_processors.tz',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-            },
         },
-    ]
+    },
+]
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -119,11 +102,10 @@ MIDDLEWARE_CLASSES = (
     'userena.middleware.UserenaLocaleMiddleware',
 )
 
-if django.VERSION >= (1, 7):
-    # Session verification will become mandatory in Django 1.10
-    MIDDLEWARE_CLASSES += (
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    )
+# Session verification will become mandatory in Django 1.10
+MIDDLEWARE_CLASSES += (
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+)
 
 # Add the Guardian and userena authentication backends
 AUTHENTICATION_BACKENDS = (
@@ -158,17 +140,7 @@ INSTALLED_APPS = (
     'userena.tests.profiles',
 )
 
-if django.VERSION < (1, 7, 0):
-    # only older versions of django require south migrations
-    INSTALLED_APPS += ('south',)
-    SOUTH_MIGRATION_MODULES = {
-        'easy_thumbnails': 'easy_thumbnails.south_migrations',
-        'userena.contrib.umessages': 'userena.contrib.umessages.south_migrations',
-    }
-
-if django.VERSION >= (1, 9, 0):
-    INSTALLED_APPS += ('easy_thumbnails',)
-
+INSTALLED_APPS += ('easy_thumbnails',)
 
 LOGGING = {
     'version': 1,
