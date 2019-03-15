827247a8de832d9be8810dfcb58fc631edeeecfa
James Meakin <code@jmsmkn.com>
2019-03-15 14:00:57 +0100

    Fix admin include

--- userena/runtests/urls.py
+++ userena/runtests/urls.py
@@ -1,23 +1,14 @@
-from django.conf.urls import url, include
 from django.conf import settings
-
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url, include
 from django.conf.urls.static import static
-
 from django.contrib import admin
-from django.core.exceptions import ImproperlyConfigured
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-try:
-    # django.VERSION < 2.0
-    admin_urls = include(admin.site.urls)
-except ImproperlyConfigured:
-    admin_urls = admin.site.urls
-
 urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', admin_urls),
+    url(r'^admin/', admin.site.urls),
 
     # Demo Override the signup form with our own, which includes a
     # first and last name.
@@ -33,5 +24,3 @@ urlpatterns = [
 # Add media and static files
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
