24e700e538753d5573846d344bae0587f5676091
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2026-02-10 12:29:40 +0100

    Update __init__.py

---
+++ userena/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "django-userena-ce is deprecated and will no longer receive updates. "
+    "Please find an alternative package for user management.",
+    DeprecationWarning,
+    stacklevel=2,
+)
