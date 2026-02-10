d2f019539b3b655cdfdb993b5ce675e08bf208b2
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2026-02-10 12:43:23 +0100

    Delete demo directory

--- demo/README.md
+++
@@ -1,4 +0,0 @@
-# Userena Demo
-
-You can use this project to see how to setup Django userena for your own
-project.
--- demo/profiles/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def promo(request):
-    return render(request, "static/promo.html")
